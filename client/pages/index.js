783108de01c3631d784d497bb126f7456bfa6c9c
Kazik Kluz <kazik.kluz@gmail.com>
2023-09-19 12:19:47 +0100

    simple server side rendering functionality, useRequest hooks, and simple stub pages

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,23 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  // const response = await axios.get('/api/users/currentuser');
+
+  // return response.data;
+  if (typeof window === 'undefined') {
+    //we are on the server
+    //request should be made to ingress-nginx
+  } else {
+    //we are on the browser
+    //request can be mande with base url of ''
+  }
+  console.log('I was executed');
+  return {};
+};
+
+export default LandingPage;
